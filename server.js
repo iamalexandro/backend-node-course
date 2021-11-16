803c343b1ff075bef4f428ab1472deb9840e11de
Nicola Di Candia <ndicandia@condorlabs.io>
2021-11-15 22:11:00 -0500

    custom headers

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
-router.get("/message", function (req, res) {
-  res.send("Messages list");
-});
+// router.get("/message", function (req, res) {
+//   res.send("Messages list");
+// });
 router.delete("/message", function (req, res) {
   console.log(req.body);
   res.send("message deleted succesfully");
@@ -18,6 +18,14 @@ router.post("/message", function (req, res) {
   console.log(req.query);
   console.log(req.body);
   res.send("message " + req.query.id + " updated succesfully");
+});
+//acces to the headers
+router.get("/headers", function (req, res) {
+  console.log(req.headers);
+  res.header({
+    "custom-header": "Our custom header",
+  });
+  res.send("Messages list");
 });
 
 app.listen(3000);
